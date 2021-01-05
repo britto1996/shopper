cababaeed3540f4c49983557014a805d2a155552
LAPTOP-FV0JU91J\BTM WORK TIME <btm15037@gmail.com>
2021-01-05 23:27:26 +0530

    no message

--- shopperprojectbackend/index.js
+++ shopperprojectbackend/index.js
@@ -1,1 +1,19 @@
-console.log("the life is crazy")
+const express = require("express")
+const app = new express()
+const port = 8000
+
+
+//db connection
+const mongoose = require("mongoose")
+
+//import created models 
+const User = require("./models/user")
+mongoose.connect('mongodb://localhost/shopperonline', {useNewUrlParser: true,
+useUnifiedTopology:true}).then(()=>{
+    console.log("DB CONNECTEED")
+})
+
+
+app.listen(port,()=>{
+    console.log(`The port is running on port ${port}`)
+})
